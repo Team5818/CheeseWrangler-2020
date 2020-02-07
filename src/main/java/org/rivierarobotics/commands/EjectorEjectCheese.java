d9c1688a1e7dc7f971cf4f2afb9a66886c635577
ryanino <ryanino2904@gmail.com>
2020-02-06 20:56:51 -0800

    Finished EjectorEjectCheese

--- src/main/java/org/rivierarobotics/commands/EjectorEjectCheese.java
+++ src/main/java/org/rivierarobotics/commands/EjectorEjectCheese.java
@@ -1,8 +1,20 @@
 package org.rivierarobotics.commands;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import net.octyl.aptcreator.Provided;
+import org.rivierarobotics.subsystems.Ejector;
 
 public class EjectorEjectCheese extends InstantCommand {
 
+    private final Ejector ejector;
 
+    public EjectorEjectCheese(@Provided Ejector ejector) {
+        this.ejector = ejector;
+        addRequirements(ejector);
+    }
+
+    @Override
+    public void execute() {
+        ejector.setPower(0.8);
+    }
 }
