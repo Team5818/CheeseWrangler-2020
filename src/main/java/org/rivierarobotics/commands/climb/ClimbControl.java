b51413b12f8aa635be3a93a9ce2fcbb2e6012bf3
Andrew-Ji10 <jiandrew10@gmail.com>
2021-01-13 11:27:31 -0800

    finished/implemented ClimbControl

---
+++ src/main/java/org/rivierarobotics/commands/climb/ClimbControl.java
@@ -0,0 +1,33 @@
+package org.rivierarobotics.commands.climb;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import org.rivierarobotics.inject.Input;
+import org.rivierarobotics.subsystems.Climb;
+import org.rivierarobotics.util.MathUtil;
+
+import javax.inject.Inject;
+
+public class ClimbControl extends CommandBase {
+    private final Climb climb;
+    private final Joystick coDriverRightJoystick;
+    private final Joystick driverButtons;
+
+
+    @Inject
+    public ClimbControl(@Input(Input.Selector.CODRIVER_RIGHT) Joystick coDriverRightJoystick, @Input(Input.Selector.CODRIVER_BUTTONS) Joystick driverButtons, Climb climb){
+        this.climb = climb;
+        this.coDriverRightJoystick = coDriverRightJoystick;
+        this.driverButtons = driverButtons;
+    }
+
+    @Override
+    public void execute(){
+        climb.setPower(MathUtil.fitDeadband(coDriverRightJoystick.getX()));
+    }
+
+    @Override
+    public boolean isFinished() {
+        return !driverButtons.getRawButton(3);
+    }
+}
